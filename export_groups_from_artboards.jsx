b3daa9c4f6f0b0721caac527ca97817dafa8f2ea
Yuri Coppe <yuricoppe@gmail.com>
2025-04-04 23:56:31 -0300

    Restaura a visibilidade original das layers

--- export_groups_from_artboards.jsx
+++ export_groups_from_artboards.jsx
@@ -25,6 +25,33 @@ if (app.documents.length > 0) {
     var layers = doc.layers;
     var artboardCount = 0;
     
+    // Store all original visibilities at the start
+    var originalVisibilities = [];
+    function storeVisibilities(layers) {
+        for (var i = 0; i < layers.length; i++) {
+            var layer = layers[i];
+            originalVisibilities.push({
+                layer: layer,
+                visible: layer.visible
+            });
+            // Store visibilities for nested layers
+            if (layer.typename === "LayerSet" && layer.layers) {
+                storeVisibilities(layer.layers);
+            }
+        }
+    }
+    
+    // Restore all visibilities at the end
+    function restoreVisibilities() {
+        for (var i = 0; i < originalVisibilities.length; i++) {
+            var item = originalVisibilities[i];
+            item.layer.visible = item.visible;
+        }
+    }
+    
+    // Store original visibilities before starting
+    storeVisibilities(layers);
+    
     // Function to check if a group is marked with a specific color
     function getGroupColor(group) {
         try {
@@ -59,161 +86,159 @@ if (app.documents.length > 0) {
         return !isLayerEmpty(layer);
     }
     
-    // Process each layer in the document
-    for (var i = 0; i < layers.length; i++) {
-        var layer = layers[i];
-        
-        // Check if the layer is an artboard (LayerSet)
-        if (layer.typename === "LayerSet") {
-            artboardCount++;
-            var artboardName = layer.name;
+    try {
+        // Process each layer in the document
+        for (var i = 0; i < layers.length; i++) {
+            var layer = layers[i];
             
-            // Get the bounds of the artboard
-            var artboardBounds = layer.bounds;
-            var left = artboardBounds[0].value;
-            var top = artboardBounds[1].value;
-            var right = artboardBounds[2].value;
-            var bottom = artboardBounds[3].value;
-            
-            // Create folder for this artboard
-            var artboardFolder = new Folder(baseFolder + "/" + artboardName);
-            if (!artboardFolder.exists) {
-                artboardFolder.create();
-            }
-            
-            // Save original visibility states for all layers
-            var originalVisibilities = [];
-            for (var k = 0; k < layers.length; k++) {
-                originalVisibilities[k] = layers[k].visible;
-                layers[k].visible = false;
-            }
-            
-            // Make the artboard visible
-            layer.visible = true;
-            
-            // Process all sublayers (groups) in this artboard
-            for (var j = 0; j < layer.layers.length; j++) {
-                var sublayer = layer.layers[j];
+            // Check if the layer is an artboard (LayerSet)
+            if (layer.typename === "LayerSet") {
+                artboardCount++;
+                var artboardName = layer.name;
                 
-                // Check if sublayer is a group
-                if (sublayer.typename === "LayerSet") {
-                    var groupColor = getGroupColor(sublayer);
-                    var groupInfo = {
-                        name: sublayer.name,
-                        artboard: artboardName,
-                        color: groupColor
-                    };
+                // Get the bounds of the artboard
+                var artboardBounds = layer.bounds;
+                var left = artboardBounds[0].value;
+                var top = artboardBounds[1].value;
+                var right = artboardBounds[2].value;
+                var bottom = artboardBounds[3].value;
+                
+                // Create folder for this artboard
+                var artboardFolder = new Folder(baseFolder + "/" + artboardName);
+                if (!artboardFolder.exists) {
+                    artboardFolder.create();
+                }
+                
+                // Hide all layers initially
+                for (var k = 0; k < layers.length; k++) {
+                    layers[k].visible = false;
+                }
+                
+                // Make the artboard visible
+                layer.visible = true;
+                
+                // Process all sublayers (groups) in this artboard
+                for (var j = 0; j < layer.layers.length; j++) {
+                    var sublayer = layer.layers[j];
                     
-                    // Skip if group is marked red and has no content
-                    if (groupColor === 'red') {
-                        if (!hasVisibleContent(sublayer)) {
-                            skippedGroups.push(groupInfo);
-                            continue;
-                        }
-                    }
-                    
-                    // Hide all groups in the current artboard
-                    for (var m = 0; m < layer.layers.length; m++) {
-                        var currentLayer = layer.layers[m];
-                        if (currentLayer.typename === "LayerSet") {
-                            currentLayer.visible = false;
-                        }
-                        // Hide Crop Mask layer if it exists
-                        if (currentLayer.name === "Crop Mask") {
-                            currentLayer.visible = false;
-                        }
-                    }
-                    
-                    // Show only the current group and its contents
-                    sublayer.visible = true;
-                    if (sublayer.layers) {
-                        for (var l = 0; l < sublayer.layers.length; l++) {
-                            var contentLayer = sublayer.layers[l];
-                            // Don't show Crop Mask layers within groups
-                            if (contentLayer.name !== "Crop Mask") {
-                                contentLayer.visible = true;
+                    // Check if sublayer is a group
+                    if (sublayer.typename === "LayerSet") {
+                        var groupColor = getGroupColor(sublayer);
+                        var groupInfo = {
+                            name: sublayer.name,
+                            artboard: artboardName,
+                            color: groupColor
+                        };
+                        
+                        // Skip if group is marked red and has no content
+                        if (groupColor === 'red') {
+                            if (!hasVisibleContent(sublayer)) {
+                                skippedGroups.push(groupInfo);
+                                continue;
                             }
                         }
+                        
+                        // Hide all groups in the current artboard
+                        for (var m = 0; m < layer.layers.length; m++) {
+                            var currentLayer = layer.layers[m];
+                            if (currentLayer.typename === "LayerSet") {
+                                currentLayer.visible = false;
+                            }
+                            // Hide Crop Mask layer if it exists
+                            if (currentLayer.name === "Crop Mask") {
+                                currentLayer.visible = false;
+                            }
+                        }
+                        
+                        // Show only the current group and its contents
+                        sublayer.visible = true;
+                        if (sublayer.layers) {
+                            for (var l = 0; l < sublayer.layers.length; l++) {
+                                var contentLayer = sublayer.layers[l];
+                                // Don't show Crop Mask layers within groups
+                                if (contentLayer.name !== "Crop Mask") {
+                                    contentLayer.visible = true;
+                                }
+                            }
+                        }
+                        
+                        // Check if group has visible content
+                        if (!hasVisibleContent(sublayer)) {
+                            blankGroups.push(groupInfo);
+                            continue;
+                        }
+                        
+                        // Create export options
+                        var exportOptions = new ExportOptionsSaveForWeb();
+                        exportOptions.format = SaveDocumentType.PNG;
+                        exportOptions.PNG8 = false;
+                        exportOptions.transparency = true;
+                        exportOptions.interlaced = false;
+                        exportOptions.quality = 100;
+                        
+                        // Create the file path
+                        var fileName = sublayer.name.replace(/[^a-zA-Z0-9]/g, "_");
+                        var filePath = new File(artboardFolder + "/" + fileName + ".png");
+                        
+                        // Store original document crop
+                        var originalCrop = doc.cropBox;
+                        
+                        // Set crop to artboard bounds
+                        doc.crop([left, top, right, bottom]);
+                        
+                        // Export the group
+                        doc.exportDocument(filePath, ExportType.SAVEFORWEB, exportOptions);
+                        
+                        // Add to exported groups
+                        exportedGroups.push(groupInfo);
+                        
+                        // Restore original crop
+                        doc.cropBox = originalCrop;
                     }
-                    
-                    // Check if group has visible content
-                    if (!hasVisibleContent(sublayer)) {
-                        blankGroups.push(groupInfo);
-                        continue;
-                    }
-                    
-                    // Create export options
-                    var exportOptions = new ExportOptionsSaveForWeb();
-                    exportOptions.format = SaveDocumentType.PNG;
-                    exportOptions.PNG8 = false;
-                    exportOptions.transparency = true;
-                    exportOptions.interlaced = false;
-                    exportOptions.quality = 100;
-                    
-                    // Create the file path
-                    var fileName = sublayer.name.replace(/[^a-zA-Z0-9]/g, "_");
-                    var filePath = new File(artboardFolder + "/" + fileName + ".png");
-                    
-                    // Store original document crop
-                    var originalCrop = doc.cropBox;
-                    
-                    // Set crop to artboard bounds
-                    doc.crop([left, top, right, bottom]);
-                    
-                    // Export the group
-                    doc.exportDocument(filePath, ExportType.SAVEFORWEB, exportOptions);
-                    
-                    // Add to exported groups
-                    exportedGroups.push(groupInfo);
-                    
-                    // Restore original crop
-                    doc.cropBox = originalCrop;
                 }
             }
-            
-            // Restore original visibility states
-            for (var k = 0; k < layers.length; k++) {
-                layers[k].visible = originalVisibilities[k];
-            }
         }
+    } finally {
+        // Always restore visibilities, even if an error occurs
+        restoreVisibilities();
+        
+        // Restore original ruler units
+        app.preferences.rulerUnits = originalRulerUnits;
+        
+        // Generate report
+        var report = "Export Report:\n\n";
+        
+        report += "Successfully Exported Groups (" + exportedGroups.length + "):\n";
+        for (var i = 0; i < exportedGroups.length; i++) {
+            var group = exportedGroups[i];
+            report += "- " + group.artboard + " > " + group.name + 
+                     (group.color === 'green' ? " (Ready)" : "") + "\n";
+        }
+        
+        report += "\nBlank Groups (" + blankGroups.length + "):\n";
+        for (var i = 0; i < blankGroups.length; i++) {
+            var group = blankGroups[i];
+            report += "- " + group.artboard + " > " + group.name + "\n";
+        }
+        
+        report += "\nSkipped Groups (" + skippedGroups.length + "):\n";
+        for (var i = 0; i < skippedGroups.length; i++) {
+            var group = skippedGroups[i];
+            report += "- " + group.artboard + " > " + group.name + " (Marked as not ready)\n";
+        }
+        
+        // Save report to file
+        var reportFile = new File(baseFolder + "/export_report.txt");
+        reportFile.open('w');
+        reportFile.write(report);
+        reportFile.close();
+        
+        alert("Export completed!\n\n" +
+              "Exported: " + exportedGroups.length + " groups\n" +
+              "Blank: " + blankGroups.length + " groups\n" +
+              "Skipped: " + skippedGroups.length + " groups\n\n" +
+              "See export_report.txt for details");
     }
-    
-    // Restore original ruler units
-    app.preferences.rulerUnits = originalRulerUnits;
-    
-    // Generate report
-    var report = "Export Report:\n\n";
-    
-    report += "Successfully Exported Groups (" + exportedGroups.length + "):\n";
-    for (var i = 0; i < exportedGroups.length; i++) {
-        var group = exportedGroups[i];
-        report += "- " + group.artboard + " > " + group.name + 
-                 (group.color === 'green' ? " (Ready)" : "") + "\n";
-    }
-    
-    report += "\nBlank Groups (" + blankGroups.length + "):\n";
-    for (var i = 0; i < blankGroups.length; i++) {
-        var group = blankGroups[i];
-        report += "- " + group.artboard + " > " + group.name + "\n";
-    }
-    
-    report += "\nSkipped Groups (" + skippedGroups.length + "):\n";
-    for (var i = 0; i < skippedGroups.length; i++) {
-        var group = skippedGroups[i];
-        report += "- " + group.artboard + " > " + group.name + " (Marked as not ready)\n";
-    }
-    
-    // Save report to file
-    var reportFile = new File(baseFolder + "/export_report.txt");
-    reportFile.open('w');
-    reportFile.write(report);
-    reportFile.close();
-    
-    alert("Export completed!\n\n" +
-          "Exported: " + exportedGroups.length + " groups\n" +
-          "Blank: " + blankGroups.length + " groups\n" +
-          "Skipped: " + skippedGroups.length + " groups\n\n" +
-          "See export_report.txt for details");
 } else {
     alert("Please open a document first!");
 } 
